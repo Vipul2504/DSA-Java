6372be459e947c962ec9764f3170899b0c3694ee
Vipul <pawanvishwakarma521@gmail.com>
2021-08-02 12:51:20 +0530

    First Program

---
+++ Hello.java
@@ -0,0 +1,14 @@
+// class Hello{
+//     public static void name(String args[]) {
+//         System.out.println("Hello");
+//     }
+// }
+class Hello
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
