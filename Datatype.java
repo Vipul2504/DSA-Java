ff7803ad4fb3f2ea5778d4807ca1840c0c0f6175
Vipul <pawanvishwakarma521@gmail.com>
2021-08-02 14:00:17 +0530

    Datatype added

---
+++ Datatype.java
@@ -0,0 +1,10 @@
+public class Datatype {
+    public static void main(String[] args) {
+        System.out.println("Integer "+Integer.BYTES);
+        System.out.println("Char "+Character.BYTES);
+        System.out.println("Short "+Short.BYTES);
+        System.out.println("Long "+Long.BYTES);
+        System.out.println("Float "+Float.BYTES);
+        System.out.println("Double "+Double.BYTES);
+    }
+}
