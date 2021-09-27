d3a2eb3c3e41187a4408d61547595e56e2fa8bcf
Vipul <pawanvishwakarma521@gmail.com>
2021-09-27 06:04:13 +0530

    array

---
+++ Leetcode/ProductOFArrayExceptSelf.java
@@ -0,0 +1,16 @@
+package Leetcode;
+public class ProductOFArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums){
+        int[] result = new int[nums.length];
+        for(int i = 0, temp = 0; i < nums.length; i++){
+            result[i] = temp;
+            temp *= nums[i];
+
+        }
+        for(int i = nums.length -1, temp =1; i >= 0; i--){
+            result[i] *= temp;
+            temp *= nums[i];
+        }
+        return result;
+    }
+}
