209d09eb7b0a3df4a1af561d9975814834aba706
Vipul <pawanvishwakarma521@gmail.com>
2021-12-28 16:07:20 +0530

    mergesortrec

---
+++ MergesortRec.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class MergesortRec {
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 2, 1 };
+        arr = MergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] MergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+
+        int Left[] = MergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int Right[] = MergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return MergeTwo(Left, Right);
+    }
+
+    static int[] MergeTwo(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+}
